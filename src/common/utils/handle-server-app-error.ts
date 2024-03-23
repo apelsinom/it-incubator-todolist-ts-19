e27acf48e59e20c47e2d7c412d1979af7059d86f
apelsinom <apelsinom@mail.ru>
2024-03-23 15:22:55 +0300

    коментарии jsDOK для функции handleServerAppError

--- src/common/utils/handle-server-app-error.ts
+++ src/common/utils/handle-server-app-error.ts
@@ -2,11 +2,18 @@ import { Dispatch } from "redux"
 import { appActions } from "app/app.reducer"
 import { BaseResponseType } from "common/types/common.types"
 
+/**
+ * handleServerAppError
+ * @param data
+ * @param dispatch
+ * @param isShowGlobalError
+ * @return ничего (void)
+ */
 export const handleServerAppError = <D>(
   data: BaseResponseType<D>,
   dispatch: Dispatch,
   isShowGlobalError: boolean = true,
-) => {
+): void => {
   if (isShowGlobalError) {
     if (data.messages.length) {
       dispatch(appActions.setAppError({ error: data.messages[0] }))
