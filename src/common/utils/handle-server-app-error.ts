852f34af11e59c7bf588b86d4b31b98d67555ad8
apelsinom <apelsinom@mail.ru>
2024-03-23 15:36:23 +0300

    коментарии jsDOK для функции handleServerAppError -> GTPchat

--- src/common/utils/handle-server-app-error.ts
+++ src/common/utils/handle-server-app-error.ts
@@ -3,11 +3,17 @@ import { appActions } from "app/app.reducer"
 import { BaseResponseType } from "common/types/common.types"
 
 /**
- * handleServerAppError
- * @param data
- * @param dispatch
- * @param isShowGlobalError
- * @return ничего (void)
+ * Функция для обработки ошибок приложения на сервере.
+ *
+ * @param {BaseResponseType<D>} data - Данные, полученные от сервера, содержащие сообщения об ошибках.
+ * @param {Dispatch} dispatch - Функция dispatch из Redux для отправки действий.
+ * @param {boolean} [isShowGlobalError=true] - Флаг, указывающий, отображать ли глобальное сообщение об ошибке или нет.
+ * @returns {void}
+ *
+ * Эта функция используется для обработки ошибок приложения на сервере путем отправки соответствующих действий для установки сообщения об ошибке и статуса приложения.
+ * Если `isShowGlobalError` равно true, будет отображено глобальное сообщение об ошибке, в противном случае будет установлен только статус приложения 'failed'.
+ * Если массив `data.messages` не пустой, будет установлено сообщение об ошибке приложения, равное первому сообщению в массиве.
+ * Если массив `data.messages` пустой, будет установлено сообщение об ошибке приложения по умолчанию 'Some error occurred'.
  */
 export const handleServerAppError = <D>(
   data: BaseResponseType<D>,
