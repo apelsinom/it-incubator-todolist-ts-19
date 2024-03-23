e608724d8591f6999253230e73fd36b518d57f44
apelsinom <apelsinom@mail.ru>
2024-03-23 19:22:45 +0300

    thunkTryCatch -> jsDOC

--- src/common/utils/thunkTryCatch.ts
+++ src/common/utils/thunkTryCatch.ts
@@ -3,7 +3,15 @@ import { handleServerNetworkError } from "common/utils/handle-server-network-err
 import { BaseThunkAPI } from "@reduxjs/toolkit/dist/createAsyncThunk"
 import { appActions } from "app/app.reducer"
 import { BaseResponseType } from "common/types"
-
+/**
+ * Функция-обёртка для обработки ошибок в асинхронных действиях.
+ *
+ * @param {BaseThunkAPI<AppRootStateType, unknown, AppDispatch, null | BaseResponseType>} thunkAPI - Объект thunkAPI, содержащий dispatch, getState и extraArgument.
+ * @param {() => Promise<T>} logic - Функция, содержащая логику асинхронного действия.
+ * @returns {Promise<T | ReturnType<typeof thunkAPI.rejectWithValue>>} - Возвращает результат выполнения функции logic в случае успеха или результат вызова rejectWithValue в случае ошибки.
+ *
+ * Эта функция используется для обработки ошибок в асинхронных действиях. Она устанавливает статус приложения 'loading' перед выполнением логики, обрабатывает ошибки с помощью функции handleServerNetworkError и устанавливает статус приложения 'idle' после выполнения логики.
+ */
 export const thunkTryCatch = async <T>(
   thunkAPI: BaseThunkAPI<AppRootStateType, unknown, AppDispatch, null | BaseResponseType>,
   logic: () => Promise<T>,
